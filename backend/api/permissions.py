d36e98eae29c4016b841ab404cb30f9233047a26
syberflea <syberlea@gmail.com>
2023-09-20 14:55:22 +0300

    Change permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,16 +1,18 @@
 from rest_framework import permissions
 
 
-class AuthorOrReadOnlyPermission(permissions.BasePermission):
-
+class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
+            or request.user.is_staff
         )
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
+            or request.user == obj.author
+        ) 
