8fc005a13636dc04d500f9dd3f073f47dd0aef15
syberflea <syberlea@gmail.com>
2023-09-29 16:38:39 +0300

    Last commit before deploy

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from .models import (
     Favorite, Ingredient, IngredientInRecipe, Recipe, ShopingCart, Tag,
 )
-from import_export.admin import ImportExportModelAdmin
 
 
 class RecipeAdmin(admin.ModelAdmin):
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.db.models import UniqueConstraint
+from django.utils.translation import gettext_lazy as _
+
 User = get_user_model()
 
 
