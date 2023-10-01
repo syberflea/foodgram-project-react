5ac63a4249a71c6689aa35bc02bc0ed49db93469
syberflea <syberlea@gmail.com>
2023-10-01 17:40:50 +0300

    DEploy #1

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/', include('api.urls')),
     path('api/', include('users.urls')),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
